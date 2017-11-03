2bfeaf186be4179021e4e4978c1c480bed92cb3c
smile <imuhao@qq.com>
2017-11-03 17:08:54 +0800

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+
+这个项目是我在博客写 Kotlin 系列教程的 Demo, 你可以点击下面的链接查看这个系列.这个系列将会持续更新.
+
+[Kotlin — Part 1:关于这个系列](http://caimuhao.com/2017/11/02/Learn-Kotlin-While-Developing-An-Android-App-Introduction/)<br/>
+[Kotlin — Part 1:配置 Android Studio](http://caimuhao.com/2017/11/03/Learn-Kotlin-While-Developing-An-Android-App-Part1/)<br/>
+[Kotlin — Part 2:语法,空安全,静态类型](http://caimuhao.com/2017/11/03/Learn-Kotlin-While-Developing-An-Android-App-Part2/)<br/>
---
+++ app/src/main/kotlin/com/caimuhao/kedditbysteps/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.caimuhao.kedditbysteps
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?): Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun changeFragment(f: Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fl_container, f)
+        ft.commit()
+    }
+}
