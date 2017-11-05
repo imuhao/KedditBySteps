cc01da4eebb3a7820bfcb921cc96c91b06603365
smile <imuhao@qq.com>
2017-11-05 17:42:56 +0800

    Data class

--- app/src/main/kotlin/com/caimuhao/kedditbysteps/fatures/news/RedditNewsItem.kt
+++ app/src/main/kotlin/com/caimuhao/kedditbysteps/fatures/news/RedditNewsItem.kt
@@ -1,9 +1,13 @@
 package com.caimuhao.kedditbysteps.fatures.news
 
+import com.caimuhao.kedditbysteps.commons.adapter.AdapterConstants
+import com.caimuhao.kedditbysteps.commons.adapter.ViewType
+
 /**
  * @author Smile
  * @time 2017/11/5  17:34
  * @desc ${TODD}
  */
-data class RedditNewsItem(var author: String, var title: String) {
+data class RedditNewsItem(var author: String, var title: String) : ViewType {
+    override fun getViewType() = AdapterConstants.NEWS
 }
