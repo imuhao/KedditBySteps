8367173a606e325bf8366a59041ae8e76f958765
smile <imuhao@qq.com>
2017-11-05 17:41:42 +0800

    Data class

---
+++ app/src/main/kotlin/com/caimuhao/kedditbysteps/fatures/news/RedditNewsItem.kt
@@ -0,0 +1,9 @@
+package com.caimuhao.kedditbysteps.fatures.news
+
+/**
+ * @author Smile
+ * @time 2017/11/5  17:34
+ * @desc ${TODD}
+ */
+data class RedditNewsItem(var author: String, var title: String) {
+}
